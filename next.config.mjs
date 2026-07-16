4a3dd1ccdd4e2da3a4e4ff6b90d6f87c9a47c632
unknown <pstmichaeljohnson721@gmail.com>
2026-07-16 01:46:06 +0100

    pdf export error fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    serverComponentsExternalPackages: ["pdfkit"],
     serverActions: {
       bodySizeLimit: "12mb"
     }
